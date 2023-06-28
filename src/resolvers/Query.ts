b125b3f63d4312944d94a2cbe05349ad1a54a839
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-06-28 16:05:21 +0700

    Query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,2 +1,37 @@
+import { Context, PostsGet } from '../types';
 
+export const Query = {
+    me: async (_: any, __: any, { prisma, userInfo }: Context) => {
+        if (!userInfo) {
+            return prisma.user.findUnique({
+                where: {
+                    id: userInfo.userId
+                }
+            })
+        }
+        return null;
+    },
+    posts: async (_: any, __: any, { prisma, userInfo }: Context) => {
+        try {
+            const posts = await prisma.post.findMany();
+            return {
+                userErrors: [],
+                posts
+            }
+        } catch (error) {
+            return {
+                userErrors: [error],
+                posts: null,
+            };
+        }
+    },
+    profile: async (_: any, { userId }: { userId: string }, { prisma }: Context) => {
+        const user = await prisma.profile.findUnique({
+            where: {
+                userId: +userId
+            }
+        });
 
+        return user;
+    }
+}
